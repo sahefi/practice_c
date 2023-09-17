def4f401cd4ab61d2d1c0cd6453222be96ed3eef
Alvianto Putra  Sahefi <87837053+sahefi@users.noreply.github.com>
2023-09-17 21:00:28 +0700

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -1,97 +1,81 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
+
+bool isNumber(char input[])
+{
+for (int i = 0; input[i] != '\0'; i++)
+{
+if (input[i] < '0' || input[i] > '9')
+{
+return false;
+}
+}
+return true;
+}
 
 int main()
 {
+// Input: Matriks A (2x2)
+int a[2][2];
+printf("Masukkan elemen matriks A (2x2):\n");
+for (int i = 0; i < 2; i++)
+{
+for (int j = 0; j < 2; j++)
+{
+char input[100];
+scanf("%s", input);
+if (!isNumber(input))
+{
+printf("Masukkan harus berupa angka.\n");
+return 1; // Exit code 1 menunjukkan kesalahan
+}
+a[i][j] = atoi(input);
+}
+}
 
-    system("cls");
-    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-    printf("=-= Program Perkalian Matrix =-=\n");
-    printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+// Input: Matriks B (2x2)
+int b[2][2];
+printf("Masukkan elemen matriks B (2x2):\n");
+for (int i = 0; i < 2; i++)
+{
+for (int j = 0; j < 2; j++)
+{
+char input[100];
+scanf("%s", input);
+if (!isNumber(input))
+{
+printf("Masukkan harus berupa angka.\n");
+return 1; // Exit code 1 menunjukkan kesalahan
+}
+b[i][j] = atoi(input);
+}
+}
 
-    // Input matriks pertama
-    int matrix1[2][2];
-    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-    printf("Masukkan Nilai Matriks Pertama\n");
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            char input[100];
-            int validInput = 0;
-            do
-            {
-                if (fgets(input, sizeof(input), stdin))
-                {
-                    if (sscanf(input, "%d", &matrix1[i][j]) == 1)
-                    {
-                        validInput = 1; // Input adalah angka yang valid
-                    }
-                    else
-                    {
-                        printf("\nInput harus berupa angka. Masukkan Ulang:\n");
-                    }
-                }
-            } while (!validInput);
-        }
-    }
+// Proses: Perkalian matriks
+int hasil[2][2];
+for (int i = 0; i < 2; i++)
+{
+for (int j = 0; j < 2; j++)
+{
+hasil[i][j] = 0;
+for (int k = 0; k < 2; k++)
+{
+hasil[i][j] += a[i][k] * b[k][j];
+}
+}
+}
 
-    // Input matriks kedua
-    int matrix2[2][2];
-    system("cls");
-    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-    printf("Masukkan Nilai Matriks Kedua\n");
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            char input[100];
-            int validInput = 0;
-            do
-            {
-                if (fgets(input, sizeof(input), stdin))
-                {
-                    if (sscanf(input, "%d", &matrix2[i][j]) == 1)
-                    {
-                        validInput = 1; // Input adalah angka yang valid
-                    }
-                    else
-                    {
-                        printf("\nInput harus berupa angka. Masukkan Ulang:\n");
-                    }
-                }
-            } while (!validInput);
-        }
-    }
+// Output: Cetak hasil perkalian matriks
+printf("Hasil perkalian matriks:\n");
+for (int i = 0; i < 2; i++)
+{
+for (int j = 0; j < 2; j++)
+{
+printf("%d ", hasil[i][j]);
+}
+printf("\n");
+}
 
-    // Proses perkalian matriks
-    int result[2][2];
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            result[i][j] = 0;
-            for (int k = 0; k < 2; k++)
-            {
-                result[i][j] += matrix1[i][k] * matrix2[k][j];
-            }
-        }
-    }
-
-    // Output hasil perkalian matriks
-    system("cls");
-    printf("=-=-=-=-=-=-=-=-=-=-=-=\n");
-    printf("Hasil perkalian matriks\n");
-    printf("-=-=-=-=-=-=-=-=-=-=-=-\n\n");
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            printf("%d ", result[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("\n");
-    return 0;
+return 0;
 }
